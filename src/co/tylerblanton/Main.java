7c3791854c0f4c58c9e745ca03ad56c1493f11de
tlblanton <tylerblanton635@gmail.com>
2016-01-26 10:38:35 -0700

    initial commit with documentation notes

---
+++ src/co/tylerblanton/Main.java
@@ -0,0 +1,20 @@
+/* MAin.java
+ *
+ * This is the main file for java version of pa1md5 for Spring 2016 Advanced Programming at the University of Colorado
+ * Denver.
+ *
+ * Author: Tyler Blanton
+ * Date Started: January 26, 2016
+ * Date Due: February 9, 2016
+ * rpository url:
+ */
+
+package co.tylerblanton;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("I haven't started yet!");
+    }
+}
